28ce7b3ad3dc061b5148f150d459ad87976959b8
NidhiPandey18 <pandeynidhi0501@gmail.com>
2026-06-17 12:45:07 +0530

    added variables practice

--- Basics/variables.py
+++ Basics/variables.py
@@ -3,4 +3,5 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-print(f"Hello, {name}!\nNext year you'll be {age + 1}.")
+print("Hello,",name)
+print("Next year you'll be",age+1)
