1a866403f2f3baf7459cb41467b9c309256efa13
NidhiPandey18 <pandeynidhi0501@gmail.com>
2026-06-18 18:28:47 +0530

    Added calculator mini project

---
+++ Mini-Projects/calculator.py
@@ -0,0 +1,19 @@
+#input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = input("Enter operator (Add/Sub/Mul/Div): ")
+
+#operation
+if operation == "Add":
+    print("Result = ",num1+num2)
+elif operation == "Sub":
+    print("Result = ",num1-num2)
+elif operation == "Mul":
+    print("Result = ",num1*num2)
+elif operation == "Div":
+    if num2 != 0:
+        print("Result = ",num1/num2)
+    else:
+        print("Division by zero error")
+else:
+    print("Invalid operator!")
