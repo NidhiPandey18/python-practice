c77fac1ccaf8ca2fcc5711eea5f4ecdf86253f79
NidhiPandey18 <pandeynidhi0501@gmail.com>
2026-06-20 14:19:44 +0530

    Added Number guessing game

---
+++ Mini-Projects/num_guessing_game.py
@@ -0,0 +1,16 @@
+# Number Guessing Game
+
+import random
+
+print("\n-----Welcome to Number Guessing Game-----\n")
+
+secret_num = random.randint(1,10)
+num = int(input("Guess the number: "))
+
+if num == secret_num :
+    print("Computer's number:",secret_num)
+    print("Congratulations!! You Guessed it right.")
+else:
+    print("Computer's number:",secret_num)
+    print("You lost :( ")
+
